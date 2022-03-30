cc4fcd9a200fd02b5eaa8c87ecf36ce548e15bfe
Mahedimm <mahedi15-12649@diu.edu.bd>
2022-03-31 01:15:09 +0600

    create post modal using framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
+import { AnimatePresence } from 'framer-motion';
+import { getSession, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState, modalTypeState } from '../atoms/modalAtom';
+import Feed from '../components/Feed';
+import Header from '../components/Header';
+import Modal from '../components/Modal';
+import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const router = useRouter();
+  const {status} = useSession({
+    required: true,
+    onUnauthenticated(){
+      router.push('/home');
+    }
+  })
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -18,10 +35,33 @@ export default function Home() {
              {/* sidebar */}
              <Sidebar />
               {/* Feed */}
+              <Feed />
           </div>
          {/* widgets */}
+         <Widgets />
+         <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
        </main>
       </div>
     </div>
   )
 }
+ export async function getServerSideProps(context) {
+    const session = await getSession(context);
+    if(!session){
+      return{
+        redirect:{
+          permanent:false,
+          destination:'/home'
+        }
+      }
+    }
+    return {
+      props: {
+        session,
+      }
+    }
+  }
